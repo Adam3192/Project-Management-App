fc0ccd0545cba195490dd9c2b5c49fed8fb86adf
Adam3192 <jeremyadixon@hotmail.com>
2022-11-04 16:28:43 -0500

    Task Management App

--- task-client/src/pages/Home.tsx
+++ task-client/src/pages/Home.tsx
@@ -2,6 +2,7 @@ import {
   IonButton,
   IonCheckbox,
   IonContent,
+  IonFooter,
   IonHeader,
   IonIcon,
   IonItem,
@@ -20,9 +21,9 @@ import axios from 'axios'
 import React from 'react'
 import { add } from 'ionicons/icons'
 import { Link } from 'react-router-dom'
+import { Dialog } from '@capacitor/dialog';
 
 const Home: React.FC = () => {
-
   useEffect(() => {
     async function getTasks() {
       await getAllTasks()
@@ -40,8 +41,36 @@ const Home: React.FC = () => {
     })
   }
 
+  async function addTask(title:string) {
+
+    let task = { title }
+    return axios
+      .post(baseUrl, task)
+      .then((response) => {
+        getAllTasks()
+        return new Promise((resolve) => resolve(response.data))
+      })
+  }
+
+  const showPrompt = async (title: string, message: string, inputText: string) => {
+    const { value } = await Dialog.prompt({
+        title,
+        message,
+        inputText
+    });
+
+    return value;
+};
+
+const prompt = async () => {
+  showPrompt('Task Title', 'Task Title', 'Enter Task').then(title => {
+      addTask(title);
+      console.log(title);
+  });
+}
+
   const displayTasks = () => {
-    return allTasks.map((task:any) => {
+    return allTasks.map((task: any) => {
       return (
         <IonItem key={task._id}>
           <IonLabel>{task.title}</IonLabel>
@@ -59,13 +88,18 @@ const Home: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-      <IonList>
+        <IonList>
           <IonListHeader>
             <IonLabel>Tasks</IonLabel>
           </IonListHeader>
           {displayTasks()}
         </IonList>
       </IonContent>
+      <IonFooter className='ion-padding'>
+          <IonButton onClick={prompt} className='ion-float-right'>
+            <IonIcon slot="icon-only" icon={add}></IonIcon>
+          </IonButton>
+      </IonFooter>
     </IonPage>
   )
 }
